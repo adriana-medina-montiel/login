36369a5516cf9bbf8c845ce416f3311b5ac5712d
adriana <adriana-medina-montiel>
2024-06-05 17:56:37 -0600

    registro angular2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private router: Router
+  ) { }
+
+ ngOnInit() {
+
+  }
+
+  redireccion(variable:number){
+    if(variable == 1)
+      this.router.navigate(['/login']);
+    else{
+      alert('Sin condiciones necesarias');
+    }
+  }
 
 }
